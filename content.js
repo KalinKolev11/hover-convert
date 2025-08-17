37fee4f4261608114e9000557d30b531af2fe34d
KalinKolev11 <kolevkalin2001@gmail.com>
2025-08-17 16:28:15 +0300

    Target currency is now loaded from storage

--- content.js
+++ content.js
@@ -1,133 +1,140 @@
 let selection = null;
-let match = null;//0 - full match, 1 - number, 2 - unit
 let originalAmount = null;
 let originalUnit = null
 
 const conversions = {
     // Length units
     meters: {
-        units: ["m", "m.", "meters", "meter", "metre", "metres"],
+        units: ["m", "m.", "meters", "meter", "metre", "metres", "метър", "метри"],
         convert: (input) => ({value: +(input * 3.28084).toFixed(2)}),
         targetUnit: "ft"
     },
     kilometers: {
-        units: ["km", "km.", "kilometers", "kilometer", "kilometre", "kilometres", "kms"],
+        units: ["km", "km.", "kilometers", "kilometer", "kilometre", "kilometres", "kms", "километър", "километри"],
         convert: (input) => ({value: +(input * 0.621371).toFixed(2)}),
         targetUnit: "mi"
     },
     centimeters: {
-        units: ["cm", "cm.", "centimeters", "centimeter", "centimetre", "centimetres", "cms"],
+        units: ["cm", "cm.", "centimeters", "centimeter", "centimetre", "centimetres", "cms", "сантиметър", "сантиметри"],
         convert: (input) => ({value: +(input / 2.54).toFixed(2)}),
         targetUnit: "in"
     },
     millimeters: {
-        units: ["mm", "mm.", "millimeter", "millimeters", "millimetre", "millimetres"],
+        units: ["mm", "mm.", "millimeter", "millimeters", "millimetre", "millimetres", "милиметър", "милиметри"],
         convert: (input) => ({value: +(input / 25.4).toFixed(2)}),
         targetUnit: "in"
     },
     miles: {
-        units: ["mi", "mi.", "miles", "mile"],
+        units: ["mi", "mi.", "miles", "mile", "мили"],
         convert: (input) => ({value: +(input * 1.60934).toFixed(2)}),
         targetUnit: "km"
     },
     yards: {
-        units: ["yd", "yd.", "yard", "yards"],
+        units: ["yd", "yd.", "yard", "yards", "ярд", "ярда", "ярдове"],
         convert: (input) => ({value: +(input * 0.9144).toFixed(2)}),
         targetUnit: "m"
     },
     feet: {
-        units: ["ft", "ft.", "foot", "feet", "'"],
+        units: ["ft", "ft.", "foot", "feet", "'", "фут", "фута", "фута"],
         convert: (input) => ({value: +(input * 0.3048).toFixed(2)}),
         targetUnit: "m",
     },
     inches: {
-        units: ["in", "in.", "inches", "inch", "\""],
+        units: ["in", "in.", "inches", "inch", "\"", "инч", "инча", "инча"],
         convert: (input) => ({value: +(input * 2.54).toFixed(2)}),
         targetUnit: "cm"
     },
 
     // Temperature units
     celsius: {
-        units: ["c", "c.", "celsius", "°c", "centigrade"],
+        units: ["c", "c.", "celsius", "°c", "centigrade", "целзий", "градуса по Целзий"],
         convert: (input) => ({value: +((input * 9 / 5) + 32).toFixed(2)}),
         targetUnit: "°F"
     },
     kelvin: {
-        units: ["k", "k.", "kelvin", "K"],
+        units: ["k", "k.", "kelvin", "K", "келвин"],
         convert: (input) => ({value: +(input - 273.15).toFixed(2)}),
         targetUnit: "°C"
     },
     fahrenheit: {
-        units: ["f", "f.", "fahrenheit", "°f"],
+        units: ["f", "f.", "fahrenheit", "°f", "фаренхайт", "градуса по Фаренхайт"],
         convert: (input) => ({value: +((input - 32) / 1.8).toFixed(2)}),
         targetUnit: "°C",
     },
 
     // Weight units
     kilograms: {
-        units: ["kg", "kg.", "kilograms", "kilogram", "kgs", "kilogramme", "kilogrammes"],
+        units: ["kg", "kg.", "kilograms", "kilogram", "kgs", "kilogramme", "kilogrammes", "килограм", "килограма", "килограми"],
         convert: (input) => ({value: +(input * 2.20462).toFixed(2)}),
         targetUnit: "lb"
     },
     grams: {
-        units: ["g", "g.", "grams", "gram", "gms"],
+        units: ["g", "g.", "grams", "gram", "gms", "грам", "грама", "грама"],
         convert: (input) => ({value: +(input * 0.035274).toFixed(2)}),
         targetUnit: "oz"
     },
     pounds: {
-        units: ["lb", "lb.", "lbs", "lbs.", "pounds", "pound"],
+        units: ["lb", "lb.", "lbs", "lbs.", "pounds", "pound", "паунд", "паунда", "паунди"],
         convert: (input) => ({value: +(input * 0.453592).toFixed(2)}),
         targetUnit: "kg"
     },
     ounces: {
-        units: ["oz", "oz.", "ounce", "ounces", "ozs"],
+        units: ["oz", "oz.", "ounce", "ounces", "ozs", "унция", "унции"],
         convert: (input) => ({value: +(input * 28.3495).toFixed(2)}),
         targetUnit: "g"
     },
     stone: {
-        units: ["st", "stone", "stones"],
+        units: ["st", "stone", "stones", "стун", "стунове"],
         convert: (input) => ({value: +(input * 6.35029).toFixed(2)}),
         targetUnit: "kg"
     }
 };
 const currencyConversions = {
-    aud: ["aud", "australian dollar", "aussie dollar", "aussie dollars", "australian dollars"],
-    brl: ["brl", "brazilian real", "reais", "real", "r$"],
-    cad: ["cad", "canadian dollar", "canadian dollars", "ca$"],
-    chf: ["chf", "swiss franc", "franc", "francs"],
-    cny: ["cny", "yuan", "renminbi", "rmb", "cn¥", "¥"],
-    czk: ["czk", "czech koruna", "koruna", "kc", "kč"],
-    dkk: ["dkk", "danish krone", "krone", "kroner", "kr"],
-    eur: ["eur", "€", "euro", "euros"],
-    gbp: ["gbp", "£", "quid"],
-    hkd: ["hkd", "hong kong dollar", "hong kong dollars", "hk$"],
-    huf: ["huf", "hungarian forint", "forint"],
-    idr: ["idr", "indonesian rupiah", "rupiah", "rp"],
-    ils: ["ils", "israeli shekel", "shekel", "shekels", "₪", "nis"],
-    inr: ["inr", "₹", "rupee", "rupees"],
-    isk: ["isk", "icelandic krona", "króna", "krónur", "kr"],
-    jpy: ["jpy", "¥", "yen"],
-    krw: ["krw", "won", "south korean won", "₩"],
-    mxn: ["mxn", "mexican peso", "peso", "pesos", "mex$"],
-    myr: ["myr", "malaysian ringgit", "ringgit", "rm"],
-    nok: ["nok", "norwegian krone", "krone", "kroner", "kr"],
-    nzd: ["nzd", "new zealand dollar", "kiwi dollar", "nz$"],
-    php: ["php", "philippine peso", "peso", "pesos", "₱"],
-    pln: ["pln", "polish zloty", "zloty", "zł"],
-    ron: ["ron", "romanian leu", "leu", "lei"],
-    rub: ["rub", "russian ruble", "ruble", "₽"],
-    sek: ["sek", "swedish krona", "krona", "kronor", "kr"],
-    sgd: ["sgd", "singapore dollar", "singapore dollars", "s$"],
-    thb: ["thb", "thai baht", "baht", "฿"],
-    try: ["try", "turkish lira", "lira", "₺", "tl"],
-    usd: ["usd", "us$", "dollar", "dollars", "buck", "bucks"],
-    zar: ["zar", "south african rand", "rand", "r"]
+    aud: ["aud", "australian dollar", "aussie dollar", "aussie dollars", "australian dollars", "австралийски долар", "австралийски долари"],
+    bgn: ["bgn", "bulgarian lev","lv.","lv", "лев", "лева", "левове"],
+    brl: ["brl", "brazilian real", "reais", "real", "r$", "бразилско реал", "бразилски реали"],
+    cad: ["cad", "canadian dollar", "canadian dollars", "ca$", "канадски долар", "канадски долари"],
+    chf: ["chf", "swiss franc", "franc", "francs", "швейцарски франк", "швейцарски франкове"],
+    cny: ["cny", "yuan", "renminbi", "rmb", "cn¥", "¥", "юан", "юани"],
+    czk: ["czk", "czech koruna", "koruna", "kc", "kč", "чешка крона", "чешки крони"],
+    dkk: ["dkk", "danish krone", "krone", "kroner", "kr", "датска крона", "датски крони"],
+    eur: ["eur", "€", "euro", "euros", "евро"],
+    gbp: ["gbp", "£", "quid", "британски паунд", "паундове"],
+    hkd: ["hkd", "hong kong dollar", "hong kong dollars", "hk$", "хонгконгски долар", "хонгконгски долари"],
+    huf: ["huf", "hungarian forint", "forint", "унгарски форинт", "форинти"],
+    idr: ["idr", "indonesian rupiah", "rupiah", "rp", "индонезийска рупия", "рупии"],
+    ils: ["ils", "israeli shekel", "shekel", "shekels", "₪", "nis", "израелски шекел", "шекели"],
+    inr: ["inr", "₹", "rupee", "rupees", "индийска рупия", "рупии"],
+    isk: ["isk", "icelandic krona", "króna", "krónur", "kr", "исландска крона", "крони"],
+    jpy: ["jpy", "¥", "yen", "йена"],
+    krw: ["krw", "won", "south korean won", "₩", "южнокорейски вон", "вон"],
+    mxn: ["mxn", "mexican peso", "peso", "pesos", "mex$", "мексиканско песо", "песо"],
+    myr: ["myr", "malaysian ringgit", "ringgit", "rm", "малайзийски рингит", "рингит"],
+    nok: ["nok", "norwegian krone", "krone", "kroner", "kr", "норвежка крона", "норвежки крони"],
+    nzd: ["nzd", "new zealand dollar", "kiwi dollar", "nz$", "новозеландски долар", "новозеландски долари"],
+    php: ["php", "philippine peso", "peso", "pesos", "₱", "филипинско песо", "песо"],
+    pln: ["pln", "polish zloty", "zloty", "zł", "полска злота", "злоти"],
+    ron: ["ron", "romanian leu", "leu", "lei", "румънски лей", "лейове"],
+    rub: ["rub", "russian ruble", "ruble", "₽", "руска рубла", "рубли"],
+    sek: ["sek", "swedish krona", "krona", "kronor", "kr", "шведска крона", "крони"],
+    sgd: ["sgd", "singapore dollar", "singapore dollars", "s$", "сингапурски долар", "долари"],
+    thb: ["thb", "thai baht", "baht", "฿", "тайландски бат", "бат"],
+    try: ["try", "turkish lira", "lira", "₺", "tl", "турска лира", "лири"],
+    usd: ["usd", "$","us$", "dollar", "dollars", "buck", "bucks", "американски долар", "долари"],
+    zar: ["zar", "south african rand", "rand", "r", "южноафрикански ранд", "рандове"]
 };
 
-const targetCurrency = "bgn";
+let targetCurrency = "bgn";//Default currency
+
+chrome.storage.sync.get(['targetCurrency'], function(result) {
+    if (result.targetCurrency) {
+        targetCurrency = result.targetCurrency.toLowerCase();
+    }
+});
 
 document.addEventListener("mouseup", function (event) {
     selection = window.getSelection().toString().trim().toLowerCase();
+    console.log(targetCurrency);
     if (selection.length === 0) return;
     getMatches();
 }, false);
@@ -138,11 +145,8 @@ document.addEventListener("selectionchange", function (event) {
 }, false)
 
 function getMatches() {
-    //const regEx = "(?<!\\S)(-?\\d+(?:[.,]\\d+)?)(?:\\s*)([a-zA-Z]+|[\\$\\€\\£\\¥\\₹])(?!\\S)";
-    //match = selection.match(regEx);
-    //if (match === null) return;
     const regEx = "(?<!\\S)(?:(-?\\d+(?:[.,]\\d+)?)(?:\\s*)([a-zA-Z]+|[\\$\\€\\£\\¥\\₹])|([a-zA-Z]+|[\\$\\€\\£\\¥\\₹])(?:\\s*)(-?\\d+(?:[.,]\\d+)?))(?!\\S)";
-    match = selection.match(regEx);
+    let match = selection.match(regEx);
     if (match===null) return;
 
     if (match[1] && match[2]) {
@@ -217,6 +221,4 @@ function showTooltip(text) {
     tooltip.style.top = `${rect.top + window.scrollY - 25}px`;
 
     document.body.appendChild(tooltip);
-
-    //setTimeout(() => tooltip.remove(), 3000);
 }
